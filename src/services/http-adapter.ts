963edbbfe7f1970fc36e95e6ae60e1a6b709c574
SirCodr <jotadeveloper00@gmail.com>
2024-08-31 20:42:30 -0500

    Refactor http adapter

--- src/services/http-adapter.ts
+++ src/services/http-adapter.ts
@@ -8,17 +8,12 @@ interface HttpInstance {
 export class Http implements HttpInstance {
   private http: AxiosInstance
 
-  constructor(baseURL: string, apiKey?: string) {
+  constructor() {
     this.http = axios.create({
-      baseURL,
+      baseURL: import.meta.env.VITE_API_URL,
       headers: {
         'Content-Type': 'application/json',
-      },
-      withCredentials: true,
-      params: {
-        apikey: apiKey,
-        corsDomain: 'localhost'
-      },
+      }
     })
   }
   async get(url: string, params: Record<string, string|number>) {
@@ -32,17 +27,3 @@ export class Http implements HttpInstance {
   }
 }
 
-export class StockApi extends Http {
-  constructor() {
-    super(import.meta.env.VITE_YAHOO_FINANCE_API_URL ?? '')
-  }
-}
-
-export class ForexApi extends Http {
-  constructor() {
-    super(
-      import.meta.env.VITE_TWELVEDATA_FOREX_API_URL ?? '',
-      import.meta.env.VITE_TWELVEDATA_FOREX_API_KEY
-    )
-  }
-}
